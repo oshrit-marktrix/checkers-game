43903e480f770be86ce6ca1b8d77c0e0bb24e9ea
Oshrit <oshrit13200@gmail.com>
2022-05-01 18:51:18 +0300

    Main arrangement of the code and preparation for the movement of the players

--- checkers.js
+++ checkers.js
@@ -1,16 +1,87 @@
-BOARD_SIZE = 8
+
+class Pieces {
+  constructor(row, col, type, player, imgUrl) {
+    this.row = row;
+    this.col = col;
+    this.type = type;
+    this.player = player;
+    this.img = this.imgToElement(imgUrl);
+  }
+  //  "tag" the img src as the player element.
+  imgToElement(url) {
+    let newEl = document.createElement("img");
+    newEl.src = url;
+    let cell = boardEl.rows[this.row].cells[this.col];
+    cell.appendChild(newEl)
+    if (this.player == 'black_player') {
+      newEl.classList.add('black')
+    }
+    return newEl;
+  }
+}
+class BoardData {//the start of the game and adding the Pieces to the board.
+  constructor(firstPlayer) {
+    this.pieces = this.getInitialPieces();
+    this.currentPlayer = firstPlayer;
+    this.winner = undefined;
+  }
+
+  clearBoard(boardEl) {// function that will delete all moves from the board
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      for (let j = 0; j < BOARD_SIZE; j++) {
+        boardEl.rows[i].cells[j].classList.remove('possible-move');
+        boardEl.rows[i].cells[j].classList.remove('selected');
+      }
+    }
+  }
+  paintPossibleMoves(piece) {// function that will "draw" all moves on the board
+    let possibleMoves = piece.getPossibleMoves();
+    for (let possibleMove of possibleMoves) {
+      let possibleCell = boardEl.rows[possibleMove[0]].cells[possibleMove[1]];
+      possibleCell.classList.add('possible-move');
+    }
+  }
+
+
+  getPiece(row, col) { // function that will "recognize" the pieces place
+    for (const piece of this.pieces) {
+      if (piece.row === row && piece.col === col) {
+        return piece
+      }
+    }
+  }
+  getInitialPieces() {// function that will place the pieces in place
+    let result = [];
+    for (let i = 0; i < 3; i += 1) {
+      for (let j = i % 2; j < 8; j += 2) {
+        result.push(new Pieces(i, j, 'black', 'black_player', imgURLsObj.blackblackImg))
+      }
+    }
+    for (let i = 5; i < 8; i += 1) {
+      for (let j = i % 2; j < 8; j += 2) {
+        result.push(new Pieces(i, j, 'white', 'white_player', imgURLsObj.whitewhiteImg))
+      }
+    }
+
+    return result;
+  }
+}
+let imgURLsObj =//img's url that get into the "pieces" class constructor
+{
+  blackblackImg: "http://chongzizil.github.io/Checkers-SMG/imgs/black_man.png",
+  blackKingImg: "http://chongzizil.github.io/Checkers-SMG/imgs/black_cro.png",
+  whitewhiteImg: "http://chongzizil.github.io/Checkers-SMG/imgs/white_man.png",
+  whiteQueenImg: "http://chongzizil.github.io/Checkers-SMG/imgs/white_cro.png",
+}
 let selectedCell;
+let boardData;
 let selectedPiece;
 
+const BOARD_SIZE = 8;
 const boardEl = document.createElement("table");
-let imgURL =
-{
-  queenblackpiece: "./piece/queenblackpiece.png",
-  whitepiece: "./PIECES/whitepiece.png",
-  queenwhitepiece: "./PIECES/queenwhitepiece.png"
-}
-function createBoard() {
-  boardEl.classList.add("Board");
+
+function createMainBoard() {
+  boardEl.classList.add("mainBoard");
   document.body.appendChild(boardEl);
   for (let row = 0; row < BOARD_SIZE; row++) {
     let rowElement = boardEl.insertRow();
@@ -21,57 +92,10 @@ function createBoard() {
       } else {
         cellElement.classList.add("black");
       }
-      cellElement.addEventListener('click', () => onCellClick(row, col));
     }
   }
-  boardData = new BoardData();
+  boardData = new BoardData('white_player');
 }
 
-window.addEventListener('load', createBoard);
-
-class Piece {
-  constructor(row, col, color, imgURL) {
-    this.row = row;
-    this.col = col;
-    this.color = color;
-    this.imgToPlayer(imgURL);
-  }
-  imgToPlayer(url) {//tag the img to player element
-    let newEl = document.createElement("img");
-    newEl.src = url;
-    let cell = boardEl.rows[this.row].cols[this.col];
-    cell.appendChild(newEl);
-    console.log(rows);
-  }
-}
-
-class BoardData {
-  constructor() {
-    this.pieces = this.getPieces();
-  }
-
-  getPlace(row, col) {
-    for (const piece of this.pieces) {
-      if (piece.row === row && piece.col === col) {
-        return piece
-      }
-    }
-  }
-  getPieces() {
-    let result = [];
-    for (let i = 0; i < 3; i += 1) {
-      for (let j = i % 2; j < 8; j += 2) {
-        result.push(new Piece(i, j, 'black', 'black_player', imgURL.blackpiece))
-      }
-    }
-    return result;
-  }
-
-  imgToPlayer() {
-    let result = [];
-    result.push(new Piece(1, 3, "blackpiece", imgURL.blackpiece))
-    result.push(new Piece(6, 2, 'whitepiece', imgURL.whitePawnImg))
-    return result;
-  }
-}
+window.addEventListener('load', createMainBoard);
 
